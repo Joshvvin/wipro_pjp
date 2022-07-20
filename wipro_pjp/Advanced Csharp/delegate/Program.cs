81a5655bcda874d327ee471033ec979c77a5f9e4
Joshvvin <74815534+Joshvvin@users.noreply.github.com>
2022-07-20 17:39:44 +0530

    Add files via upload

---
+++ wipro_pjp/Advanced Csharp/delegate/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+public delegate void deladd(int a, int b);
+public delegate void deldiv(int a, int b);
+public delegate void delsub(int a, int b);
+public delegate void delmul(int a, int b);
+class Program
+    {
+        public static void Main(string[] args)
+        {
+        Console.WriteLine("Enter the first number: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Enter the second number: ");
+        int b = int.Parse(Console.ReadLine());
+        deladd da = new deladd(add);
+        da(a, b);
+        delsub ds = new delsub(sub);
+        ds(a, b);
+        delmul dm = new delmul(mul);
+        dm(a, b);
+        deldiv dd = new deldiv(div);
+        dd(a, b);
+    }
+        public static void add(int a, int b)
+        {
+        int result = a + b;
+        Console.WriteLine( a+" + " + b + " is : " + result);
+        }
+        public static void sub(int a, int b)
+        {
+            int result = a - b;
+            Console.WriteLine(a + " - " + b + " is : " + result);
+        }
+        public static void mul(int a, int b)
+        {
+        int result = a * b;
+        Console.WriteLine(a + " * " + b + " is : " + result);
+        }
+        public static void div(int a, int b)
+        {
+        int result = a / b;
+        Console.WriteLine(a + " / " + b + " is : " + result);
+        }
+}
+
---
+++ wipro_pjp/Advanced Csharp/reflection/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection;
+
+namespace reflection
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Type t = Type.GetType("reflection.Customer");
+            Console.WriteLine("*** " + t.AssemblyQualifiedName);
+            Console.WriteLine("***** " + t.Namespace);
+            Console.WriteLine("******* " + t.Name);
+            ConstructorInfo[] constructors = t.GetConstructors();
+            foreach(ConstructorInfo constructor in constructors)
+            {
+                Console.WriteLine("********* " + constructor.ToString());               
+            }            
+            PropertyInfo[] properties = t.GetProperties();
+            foreach (PropertyInfo property in properties)
+            {
+                Console.WriteLine("********* " + property.ToString());                
+            }          
+            MethodInfo[] methods = t.GetMethods();
+            foreach (MethodInfo method in methods)
+            {
+                Console.WriteLine("********* " + method.ToString());                
+            }            
+        }
+    }
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+        public Customer()
+        {
+            this.Id = -1;
+            this.Name = string.Empty;
+        }
+        public void printid()
+        {
+            Console.WriteLine("Id : " + this.Id);
+        }
+        public void printname()
+        {
+            Console.WriteLine("Name : " + this.Name);
+        }
+    }
+}
